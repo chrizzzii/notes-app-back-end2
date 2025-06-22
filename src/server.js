a60acddc3425597afc312af2725121e8e02740d1
chrizzzii <clvtito@gmail.com>
2025-06-22 12:57:16 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,14 +27,55 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
+  const ClientError = require('./exceptions/ClientError');
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof Error) {
+      // Client error (custom error seperti ValidationError)
+      if (response instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: response.message,
+        }).code(response.statusCode);
+      }
+
+      // Error dari Hapi seperti 404
+      if (response.isBoom && response.output?.statusCode === 404) {
+        return h.response({
+          status: 'fail',
+          message: 'Resource tidak ditemukan',
+        }).code(404);
+      }
+
+      // Server error
+      console.error(response);
+      return h.response({
+        status: 'error',
+        message: 'Terjadi kegagalan pada server kami.',
+      }).code(500);
+    }
+
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
