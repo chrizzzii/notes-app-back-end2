62ccf48f2cfc4b328e5221b4baa9d79cbe74461b
chrizzzii <clvtito@gmail.com>
2025-06-24 14:34:42 +0700

    S3 storage implemented fixed

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -1,28 +1,26 @@
-const ClientError = require('../../exceptions/ClientError');
-
 class UploadsHandler {
-    constructor(service, validator) {
-        this._service = service;
-        this._validator = validator;
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
 
-        this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
-    }
+    this.postUploadImageHandler = this.postUploadImageHandler.bind(this);
+  }
 
-    async postUploadImageHandler(request, h) {
-        const { data } = request.payload;
-        this._validator.validateImageHeaders(data.hapi.headers);
+  async postUploadImageHandler(request, h) {
+    const { data } = request.payload;
+    this._validator.validateImageHeaders(data.hapi.headers);
 
-        const fileLocation = await this._service.writeFile(data, data.hapi);
+    const fileLocation = await this._service.writeFile(data, data.hapi);
 
-        const response = h.response({
-            status: 'success',
-            data: {
-                fileLocation,
-            },
-        });
-        response.code(201);
-        return response;
-    }
+    const response = h.response({
+      status: 'success',
+      data: {
+        fileLocation,
+      },
+    });
+    response.code(201);
+    return response;
+  }
 }
 
 module.exports = UploadsHandler;
--- src/api/uploads/index.js
+++ src/api/uploads/index.js
@@ -2,10 +2,10 @@ const UploadsHandler = require('./handler');
 const routes = require('./routes');
 
 module.exports = {
-    name: 'uploads',
-    version: '1.0.0',
-    register: async (server, { service, validator }) => {
-        const uploadsHandler = new UploadsHandler(service, validator);
-        server.route(routes(uploadsHandler));
-    },
+  name: 'uploads',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const uploadsHandler = new UploadsHandler(service, validator);
+    server.route(routes(uploadsHandler));
+  },
 };
